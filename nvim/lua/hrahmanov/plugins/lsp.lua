121b457c145fd2db4235519e6e9b9cf922646c46
Hasan Rahmanov <h.rahmanov@billrun.com>
2024-04-09 19:39:39 +0400

    terraform lsp fix

--- nvim/lua/hrahmanov/plugins/lsp.lua
+++ nvim/lua/hrahmanov/plugins/lsp.lua
@@ -65,11 +65,17 @@ return {
 						},
 					})
 				end,
+				["terraformls"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.terraformls.setup({
+						capabilities = capabilities,
+					})
+				end,
 				["yamlls"] = function()
 					local lspconfig = require("lspconfig")
 					lspconfig.yamlls.setup({
 
-						on_attach = on_attach,
+						-- on_attach = on_attach,
 						capabilities = require("cmp_nvim_lsp").default_capabilities(
 							vim.lsp.protocol.make_client_capabilities()
 						),
