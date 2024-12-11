7b3a6f966442d3da15eba0383425a6dd06fa7bf1
Hasan Rahmanov <hasan.rahmanov@12go.asia>
2024-12-11 20:51:10 +0400

    deleted multigrep

--- nvim/lua/hrahmanov/plugins/telescope.lua
+++ nvim/lua/hrahmanov/plugins/telescope.lua
@@ -13,7 +13,7 @@ return {
       require("telescope").setup({
         pickers = {
           find_files = {
-            theme = "ivy",
+            theme = "dropdown",
           },
         },
         extensions = {
@@ -26,12 +26,13 @@ return {
 
       require("telescope").load_extension("ui-select")
       require("telescope").load_extension("fzf")
-      require("hrahmanov.plugins.multigrep").setup()
+      -- require("hrahmanov.plugins.multigrep").setup()
 
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[S]earch [H]elp" })
       vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
       vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[ ] Find existing buffers" })
       vim.keymap.set("n", "<leader>fS", builtin.git_status, { desc = "" })
